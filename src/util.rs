75ea464af9ed9d749899fa762ec31cd4c16bbab2
Ross Sullivan <rosssullivan101@gmail.com>
2026-09-07 00:27:14 +0900

    remove num_cpus

--- src/util.rs
+++ src/util.rs
@@ -2,5 +2,8 @@ pub fn cpu_count() -> usize {
     // We are going to be IO bound most of the time.
     // Reducing the number of threads really helps keep memory usage down while not making a huge
     // difference on the overall performance.
-    num_cpus::get().clamp(1, 6)
+    std::thread::available_parallelism()
+        .map(|v| v.get())
+        .unwrap_or(1)
+        .clamp(1, 6)
 }
